02ad297fc7c01d3e73a95e90e79f9384b3c78d12
Maxwell-Axfi <lelia.vaultier@gmail.com>
2023-02-25 16:47:00 +0200

    Intégration footer

---
+++ app/public/wp-content/themes/blankslate-child/footer.php
@@ -0,0 +1,13 @@
+</main>
+</div>
+
+<footer id="footer" role="contentinfo">
+    <?php wp_nav_menu( array( 'theme_location' => 'menu-footer', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+</footer>
+
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
