f8300fda6eb6bdb7c511cabf44463e875830c0e1
Maxwell-Axfi <lelia.vaultier@gmail.com>
2023-06-09 14:29:20 +0200

    Simplification des fichiers php des pages (1 élément inutile)

--- app/public/wp-content/themes/blankslate-child/commander.php
+++ app/public/wp-content/themes/blankslate-child/commander.php
@@ -5,8 +5,6 @@
 
 get_header(); ?>
 
-<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-
 <?php the_content(); ?>
 
 <script>
@@ -44,5 +42,4 @@ jQuery('.quantity').each(function() {
 });
 </script>
 
-<?php endwhile; endif; ?>
 <?php get_footer(); ?>
